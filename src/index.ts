ecf62506244d942969679305ebcfbc2a52625b6b
Alan Oliver <alanoliver@R00067.local>
2023-10-02 12:34:24 +0100

    export http error, member guard and processor name

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from "./scanner-core.module";
 export * from "./services/cluster.service";
 export * from "./processor/scan.processor";
+
+export { PROCESSOR_NAME } from "./processor/scan.processor";
+export { LockedHttpException } from "./errors/custom-http-exceptions";
+export { MemberGuard } from "./guards/member.guard";
